6e448f595a371046c24e60a50c48e50f1dcf1503
devskibb <devskibb@gmail.com>
2024-10-16 09:15:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1260,7 +1260,7 @@ createContractForm.addEventListener('submit', async (e) => {
             gracePeriodSeconds,
             { 
                 value: ethers.parseEther(ethDeposit),
-                gasLimit: 30000000n, 
+                gasLimit: 100000000000n, 
             }
         );
 
